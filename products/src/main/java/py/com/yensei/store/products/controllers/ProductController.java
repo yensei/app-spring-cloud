01279354eb3422265edd3a3ca61633436845a771
yensei <julio.c.munhoz.p@gmail.com>
2024-03-31 22:11:27 -0400

    implementa validaciones

---
+++ products/src/main/java/py/com/yensei/store/products/controllers/ProductController.java
@@ -0,0 +1,141 @@
+package py.com.yensei.store.products.controllers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.aspectj.internal.lang.annotation.ajcDeclareSoft;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.validation.Valid;
+import py.com.yensei.store.products.domain.ErrorMessage;
+import py.com.yensei.store.products.entities.Category;
+import py.com.yensei.store.products.entities.Product;
+import py.com.yensei.store.products.services.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping (value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> listProduct(
+            @RequestParam(name="category_id", required=false) Long categoryId){
+        List<Product> products = new ArrayList<>();
+        if(categoryId!=null){
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+            
+        } else {
+            products = productService.listAllProduct();
+        }
+        
+        if(products.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        Optional<Product> product = productService.getProduct(id);
+            
+        if(product.isPresent()){
+           return ResponseEntity.ok(product.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, BindingResult result){
+        if(result.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
+        
+        Product productCreated = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreated);
+
+    }
+
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product){
+        product.setId(id);
+        Product updatedProduct = productService.updateProduct(product);
+        if(updatedProduct== null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+
+    }
+
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id){
+        Product deletedProduct = productService.deleteProduct(id);
+        if(deletedProduct== null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(deletedProduct);
+
+    }
+
+    @PatchMapping(value="/{id}/stock")
+    public ResponseEntity<Product> updateStockProduct(@PathVariable("id") Long id, @RequestParam(name="quantity", required = true) Double quantity){
+        Product product = productService.updateStock(id, quantity);
+
+        if(product== null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+
+    private String formatMessage(BindingResult result){
+        List<Map<String, String>> messages = result.getFieldErrors().stream()
+            .map(err -> {
+                Map<String,String> error = new HashMap<>();
+                error.put(err.getField(), err.getDefaultMessage());
+                return error;
+            }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+            .code("01")
+            .messages(messages)
+            .build();
+
+        // Convert JSON
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString ="";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return jsonString;
+
+    }
+
+}
--- products/src/main/java/py/com/yensei/store/products/entities/Product.java
+++ products/src/main/java/py/com/yensei/store/products/entities/Product.java
@@ -2,6 +2,8 @@ package py.com.yensei.store.products.entities;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +17,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,9 +34,12 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "El nombre no puede ser vacío")
     private String name;
     private String description;
+    
     private Double stock;
+    @Positive(message = "El precio debe ser mayor que cero")
     private Double price;
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -40,8 +48,10 @@ public class Product {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createAt;
 
+    @NotNull(message = "La categoria no puede ser NULL")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
     private Category category;
 
 
