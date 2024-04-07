a756fed48ac0dd51076adff67f7570ae16c2adc8
yensei <julio.c.munhoz.p@gmail.com>
2024-04-07 18:19:00 -0400

    Crea proyecto utils para evitar repetir codigo

---
+++ utils/src/main/java/py/com/yensei/store/App.java
@@ -0,0 +1,13 @@
+package py.com.yensei.store;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ utils/src/main/java/py/com/yensei/store/utils/constants/Status.java
@@ -0,0 +1,6 @@
+package py.com.yensei.store.utils.constants;
+
+public enum Status {
+    CREATED, DELETED
+
+}
