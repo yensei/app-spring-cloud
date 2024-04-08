0eb561eaa625971d150791cdce6d67c4c1ff4761
yensei <julio.c.munhoz.p@gmail.com>
2024-04-08 03:08:14 -0400

    implementa euraka server y configura los clients

--- customers/src/main/java/py/com/yensei/store/customers/CustomersApplication.java
+++ customers/src/main/java/py/com/yensei/store/customers/CustomersApplication.java
@@ -2,7 +2,9 @@ package py.com.yensei.store.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomersApplication {
 
