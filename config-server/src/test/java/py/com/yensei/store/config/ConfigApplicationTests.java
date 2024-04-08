cf148401058de7eee550d54da3e8e066830d69b3
yensei <julio.c.munhoz.p@gmail.com>
2024-04-07 22:55:00 -0400

    crea el config server

---
+++ config-server/src/test/java/py/com/yensei/store/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.yensei.store.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
