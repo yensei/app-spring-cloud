3680dfe81f6b93e366e19623785b7d9b6d0dd713
yensei <julio.c.munhoz.p@gmail.com>
2024-04-21 02:49:07 -0400

    eliminate anotation que ya no son necesarios

--- products/src/main/java/py/com/yensei/store/products/ProductsApplication.java
+++ products/src/main/java/py/com/yensei/store/products/ProductsApplication.java
@@ -2,10 +2,8 @@ package py.com.yensei.store.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ProductsApplication {
 
 	public static void main(String[] args) {
--- shopping/src/main/java/py/com/yensei/store/shopping/ShoppingApplication.java
+++ shopping/src/main/java/py/com/yensei/store/shopping/ShoppingApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -15,7 +14,6 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 public class ShoppingApplication {
 
