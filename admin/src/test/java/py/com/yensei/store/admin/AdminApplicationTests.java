877e4a408d5e84e278358830911befbe42a96143
yensei <julio.c.munhoz.p@gmail.com>
2024-04-21 04:14:55 -0400

    implementa admin para observalidad

---
+++ admin/src/test/java/py/com/yensei/store/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.yensei.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
